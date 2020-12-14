be0896c5825b72c8b3b6d6d2367a44e7f11c2ceb
BoomRocker <boomrocker@gmail.com>
2020-12-14 08:00:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 	var countP=0
 	var countW=0
 	var countA=0
-	var countS=0	
+	var countS=0
+	var countM=0
+	var countN=0
 	var woundWatcher=0
+	var poisonWatcher=0
+	var attackMulti=0
+	var attackNull=0
 	
 	var woundFinal=0
 	var poisonFinal=0
@@ -28,29 +33,30 @@
 			document.getElementById("totalSHLD").innerHTML="shield "+countS;
 			document.getElementById("totalPSN").innerHTML="poison "+countP;
 			
-			document.getElementById("poisonSwitch").addEventListener("load", function(){
-				if(countP > 0){
-					document.getElementById("poisonDMGcount").style.opacity = "1";
-			   } else {
-					document.getElementById("poisonDMGcount").style.opacity = "0";
-				}
-				}
-			);
+			document.getElementById("attackNull").disabled = true;
+			document.getElementById("poisonSwitch").disabled = true;
+			document.getElementById("attackMultiply").disabled = true;
+
+			
+
 		
 		// Listener for Attack Poisoner START
 		document.getElementById("attackAdd").addEventListener("load", function(){
 			if(countA > 0){
 					document.getElementById("poisonSwitch").disabled = false;
+					document.getElementById("attackMultiply").disabled = false;
+					document.getElementById("attackNull").disabled = false;
 			   } else {
 					document.getElementById("poisonSwitch").disabled = true;
+					document.getElementById("attackMultiply").disabled = true;
+					document.getElementById("attackNull").disabled = true;
 				}
 			}		
 		); // Listener for Attack Poisoner END
 		
-		// Toggle Switch for Poison START
+/*		// Toggle Switch for Poison START
 		document.getElementById("poisonSwitch").addEventListener("click", function(){
 			if(document.getElementById("poisonSwitch").checked == true){
-				document.getElementById("poisonHurts").style.color = "lime";
 				countP=countP+1;
 				count=count+1;
 				  document.getElementById("damageTotaler").innerHTML=count;
@@ -60,10 +66,30 @@
 				   	countP=countP-1;
 					count=count-1;
 					  document.getElementById("damageTotaler").innerHTML=count;
-					  document.getElementById("poisonDMGcount").style.opacity = "0";
+					  document.getElementById("poisonDMGcount").style.opacity = "1";
 				}
 			}		
 		); // Toggle Switch for Wound END
+*/
+	// Toggle Switch for POISON 2 START
+		document.getElementById("poisonSwitch").addEventListener("change", function(){
+			if(document.getElementById("poisonSwitch").checked == true){
+					poisonWatcher=1;
+					count=count+poisonWatcher;
+					document.getElementById("damageTotaler").innerHTML=count;
+				  	countP=1;
+					document.getElementById("poisonDMGcount").innerHTML=countP;
+					document.getElementById("attackSubtract").disabled = true;
+			   } else {
+				   	count=count-poisonWatcher;
+					document.getElementById("damageTotaler").innerHTML=count;
+					poisonWatcher=0;
+					countP=0;
+					document.getElementById("poisonDMGcount").innerHTML=countP;
+					document.getElementById("attackSubtract").disabled = false;
+				}
+			}		
+		); // Toggle Switch for POISON 2 END
 		
 		// Toggle Switch for Wound START
 		document.getElementById("woundSwitch").addEventListener("change", function(){
@@ -111,21 +137,70 @@
 		document.getElementById("attackAdd").onclick=function(){
 			countA=countA+1;
 			document.getElementById("opacityP").style.opacity = "1";
+			document.getElementById("attackMultiply").style.opacity = "1";
+			document.getElementById("attackNull").style.opacity = "1";
 			countFloat=Math.max(0,countA+countP-countS);		
 			document.getElementById("attackDamage").innerHTML=countA;
 			count=countFloat;
 			document.getElementById("damageTotaler").innerHTML=count;
 			document.getElementById("poisonSwitch").disabled = false;
+			document.getElementById("attackMultiply").disabled = false;
+			document.getElementById("attackNull").disabled = false;
 		}
 		document.getElementById("attackSubtract").onclick=function(){
 			countA=Math.max(0, countA-1)
 			countFloat=Math.max(0,countA+countP-countS);	
-		document.getElementById("attackDamage").innerHTML=countA;
+			document.getElementById("attackDamage").innerHTML=countA;
 			countFloat=Math.max(0,countA-countS);
 			count=countFloat;
-		document.getElementById("damageTotaler").innerHTML=count;
-		document.getElementById("poisonSwitch").disabled = true;
+			document.getElementById("damageTotaler").innerHTML=count;
+
+		   
+	   }
+	   
+//MULTIPLY NULL START
+		document.getElementById("attackMultiply").onclick=function(){		
+			if (countM == 1 ){
+				document.getElementById("attackMultiply").style.backgroundColor = "transparent";
+				document.getElementById("attackMultiply").style.color = "#000";
+				var modifyFlipM=".";
+				document.getElementById("modifyDamage").innerHTML=modifyFlipM;
+				countM=0;
+			} else{
+				countM=1;
+				countA=countA*2;
+				var modifyFlipM="x2";
+				document.getElementById("attackMultiply").style.backgroundColor = "#7e57a6";
+				document.getElementById("attackMultiply").style.color = "#fff";
+				document.getElementById("modifyDamage").innerHTML=modifyFlipM;
+				document.getElementById("modifyDamage").style.color = "#7e57a6";
+				count=countFloat;
+				document.getElementById("damageTotaler").innerHTML=count;
+				document.getElementById("attackDamage").innerHTML=countA;
+			}
 		}
+		document.getElementById("attackNull").onclick=function(){
+			if (countN == 1 ){
+				document.getElementById("attackNull").style.backgroundColor = "transparent";
+				document.getElementById("attackNull").style.color = "#000";
+				countN=0;
+			} else{
+				countN=1;
+				countA=countA*0;
+				var modifyFlipN="X";
+				document.getElementById("attackNull").style.backgroundColor = "orange";
+				document.getElementById("attackNull").style.color = "#fff";
+				document.getElementById("modifyDamage").innerHTML=modifyFlipN;
+				document.getElementById("modifyDamage").style.color = "#c78e00";
+				count=countFloat;
+				document.getElementById("damageTotaler").innerHTML=count;
+				document.getElementById("attackMultiply").style.opacity = ".5";
+				/*document.getElementById("attackDamage").innerHTML=countA;*/
+			}
+
+		   
+	   }
+		
 		
 //RESOLVE START+
 		document.getElementById("resolve").onclick=function(){
